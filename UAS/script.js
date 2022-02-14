78b3b8126f048c2b0baa1a0c153b3a6199342804
fluctlightcard <fluctlightcard@gmail.com>
2022-02-15 00:03:49 +0800

    first commit

---
+++ UAS/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(".hide").click(function() {
+    $(".text").hide("fast");
+  });
+
+  $(".show").click(function() {
+    $(".text").show("fast");
+  });
+
+});
